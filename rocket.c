f5da4afb131b7f969cdc80a1c7e53e9366e6af97
DanTySmall <Danieltysmall@knights.ucf.edu>
2024-03-08 16:16:11 -0500

    Peter Fast

--- rocket.c
+++ rocket.c
@@ -4,6 +4,7 @@
 
 int main(int argc, char const *argv[]) {
   printf("Hello, World!\n" );
-  printf("My Name is Daniel\n");
+  printf("My Name is Peter\n");
+  printf("I am Going 300 mph");
   return 0;
 }
